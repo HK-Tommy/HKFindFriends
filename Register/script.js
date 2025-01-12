72cc477ce64a09d7739e69c2acc1daeb7b0ebbee
HK 湯米Tommy <tlamhappy10@gmail.com>
2025-01-12 13:18:17 +0800

    Add

---
+++ Register/script.js
@@ -0,0 +1,39 @@
+const validateEmail = (email) => {
+    return email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  };
+
+function CheckPassword(inputtxt) { 
+    return inputtxt.match(
+        /^[A-Za-z]\w{7,14}$/
+    )
+};
+
+function Complete() {
+    Email = document.getElementById("Input_Email").value
+    Passwords = document.getElementById("Input_Passwords").value
+    ComformPasswords = document.getElementById("Input_ComfirmPasswords").value
+
+    if (Email != null && Passwords !=null && ComformPasswords != null)
+        if (validateEmail(Email)){
+            if (Passwords === ComformPasswords){
+                if (CheckPassword(Passwords)){
+                     window.location = window.location + '?email=' + Email + '&passwords=' + Passwords
+                }
+                else {
+                    window.alert("密碼太弱了 ! 請重新設定 ! ")
+                    document.getElementById("Input_Passwords").value = ''
+                    document.getElementById("Input_ComfirmPasswords").value = ''
+                }
+            }
+            else{
+                window.alert("兩種密碼不相同 ! 請重新輸入 ! ")
+                document.getElementById("Input_ComfirmPasswords").value = ''
+            }
+        }
+        else{
+            window.alert("請輸入正確的電郵地址 ! ")
+            document.getElementById("Input_Email").value = ''
+        }
+    }
